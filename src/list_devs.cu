741b6529a4592c5c257e69809a0d401f8d15bc3c
spagdoon0411 <spandan.suthar@live.com>
2025-06-28 00:10:27 +0000

    error-check list_devs

--- src/list_devs.cu
+++ src/list_devs.cu
@@ -6,7 +6,11 @@ int main() {
   cudaGetDeviceCount(&deviceCount);
   for (int i = 0; i < deviceCount; ++i) {
     cudaDeviceProp prop;
-    cudaGetDeviceProperties(&prop, i);
+    cudaError_t res = cudaGetDeviceProperties(&prop, i);
+    if (res != cudaSuccess) {
+      printf("Error getting properties for device %d\n", i);
+      continue;
+    }
     printf("Device %d: %s\n", i, prop.name);
   }
   return 0;
